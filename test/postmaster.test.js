92a60566bb2195f55cf0e9685a16c9a36b8faa25
bwhatley <bwhatley@BWHATLEY-LPTP3>
2017-02-08 14:22:41 -0500

    Remove the old dead-letter test file.

--- test/postmaster.test.js
+++ test/postmaster.test.js
@@ -230,140 +230,249 @@ describe('publisher functions', () => {
 	//		spyConsume.should.have.been.calledOnce();
 	//	}));
 	//});
-
-	describe('listener tests', function () {
-		let postmaster;
-		let sandbox;
-
-		before(() => {
-			postmaster = new postmasterGeneral.Postmaster();
-			return postmaster.start();
-		});
-
-		beforeEach(function () {
-			sandbox = sinon.sandbox.create();
-		});
-
-		after(() => {
-			postmaster.stop();
-		});
-
-		afterEach(function () {
-			sandbox.restore();
-		});
-
-		//describe('handleMessage()', function () {
-		//	it('should not handle empty messages', Sinon.test(function () {
-		//		// stubs
-		//		var stubHandleRequest = this.stub(listener.utils, 'handle_request', function (seneca, data, options, cb) {
-		//			return cb();
-		//		});
-
-		//		// spies
-		//		var spyStringifyJSON = this.spy(listener.utils, 'stringifyJSON');
-		//		var spySendToQueue = this.spy(transport.channel, 'sendToQueue');
-		//		var spyAck = this.spy(transport.channel, 'ack');
-
-		//		// handle the message
-		//		listener.handleMessage(message, data);
-
-		//		/*
-		//		 * assertions
-		//		 */
-		//		stubHandleRequest.should.have.been.calledOnce();
-		//		spyStringifyJSON.should.have.not.been.called();
-		//		spySendToQueue.should.have.not.been.called();
-		//		spyAck.should.have.not.been.called();
-		//	}));
-
-		//	it('should push messages to reply queue and acknowledge them', Sinon.test(function () {
-		//		// stubs
-		//		var stubHandleRequest = this.stub(listener.utils, 'handle_request', function (seneca, data, options, cb) {
-		//			return cb(data);
-		//		});
-
-		//		// spies
-		//		var spyStringifyJSON = this.spy(listener.utils, 'stringifyJSON');
-		//		var spySendToQueue = this.spy(transport.channel, 'sendToQueue');
-		//		var spyAck = this.spy(transport.channel, 'ack');
-
-		//		// handle the message
-		//		listener.handleMessage(message, data);
-
-		//		/*
-		//		 * assertions
-		//		 */
-		//		stubHandleRequest.should.have.been.calledOnce();
-		//		spyStringifyJSON.should.have.been.calledOnce();
-		//		spyStringifyJSON.should.have.been.calledWithExactly(seneca, 'listen-amqp', data);
-		//		spySendToQueue.should.have.been.calledOnce();
-		//		spySendToQueue.should.have.been.calledWithExactly(message.properties.replyTo, new Buffer(JSON.stringify(data)), {
-		//			correlationId: message.properties.correlationId
-		//		});
-		//		spyAck.should.have.been.calledOnce();
-		//		spyAck.should.have.been.calledWithExactly(message);
-		//	}));
-		//});
-
-		//describe('listen()', function () {
-		//	it('should listen and consume messages from the channel', Sinon.test(function () {
-		//		// stubs
-		//		var spyConsume = this.stub(postmaster.listenerConn.channel, 'consume', function (queue, cb) {
-		//			// return the message
-		//			return cb(message);
-		//		});
-
-		//		listener.listenForMessages(postmaster.listenerConn, message);
-
-		//		spyConsume.should.have.been.calledOnce();
-		//	}));
-		//});
-
-		//describe('consume()', function () {
-		//	it('should not acknowledge messages without content', Sinon.test(function () {
-		//		var msg = {
-		//			properties: {
-		//				replyTo: 'seneca.res.r1FYNSEN'
-		//			}
-		//		};
-
-		//		var spyNack = this.spy(transport.channel, 'nack');
-		//		var spyHandleMessage = this.spy(listener, 'handleMessage');
-
-		//		// consume the message
-		//		listener.consume()(msg);
-
-		//		spyNack.should.have.been.calledOnce();
-		//		spyHandleMessage.should.not.have.been.called();
-		//	}));
-
-		//	it('should not acknowledge messages without a replyTo property', Sinon.test(function () {
-		//		var msg = {
-		//			properties: {}
-		//		};
-
-		//		var spyNack = this.spy(transport.channel, 'nack');
-		//		var spyHandleMessage = this.spy(listener, 'handleMessage');
-
-		//		// consume the message
-		//		listener.consume()(msg);
-
-		//		spyNack.should.have.been.calledOnce();
-		//		spyHandleMessage.should.not.have.been.called();
-		//	}));
-
-		//	it('should handle a valid message', Sinon.test(function () {
-		//		var spyNack = this.spy(transport.channel, 'nack');
-		//		var spyHandleMessage = this.spy(listener, 'handleMessage');
-		//		var spyParseJSON = this.spy(listener.utils, 'parseJSON');
-
-		//		// consume the message
-		//		listener.consume()(message);
-
-		//		spyNack.should.not.have.been.called();
-		//		spyParseJSON.should.have.been.calledOnce();
-		//		spyHandleMessage.should.have.been.calledOnce();
-		//	}));
-		//});
-	});
 });
+
+describe('listener tests', function () {
+	let postmaster;
+	let sandbox;
+
+	before(() => {
+		postmaster = new postmasterGeneral.Postmaster();
+		return postmaster.start();
+	});
+
+	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+	});
+
+	after(() => {
+		postmaster.stop();
+	});
+
+	afterEach(function () {
+		sandbox.restore();
+	});
+
+	//describe('handleMessage()', function () {
+	//	it('should not handle empty messages', Sinon.test(function () {
+	//		// stubs
+	//		var stubHandleRequest = this.stub(listener.utils, 'handle_request', function (seneca, data, options, cb) {
+	//			return cb();
+	//		});
+
+	//		// spies
+	//		var spyStringifyJSON = this.spy(listener.utils, 'stringifyJSON');
+	//		var spySendToQueue = this.spy(transport.channel, 'sendToQueue');
+	//		var spyAck = this.spy(transport.channel, 'ack');
+
+	//		// handle the message
+	//		listener.handleMessage(message, data);
+
+	//		/*
+	//		 * assertions
+	//		 */
+	//		stubHandleRequest.should.have.been.calledOnce();
+	//		spyStringifyJSON.should.have.not.been.called();
+	//		spySendToQueue.should.have.not.been.called();
+	//		spyAck.should.have.not.been.called();
+	//	}));
+
+	//	it('should push messages to reply queue and acknowledge them', Sinon.test(function () {
+	//		// stubs
+	//		var stubHandleRequest = this.stub(listener.utils, 'handle_request', function (seneca, data, options, cb) {
+	//			return cb(data);
+	//		});
+
+	//		// spies
+	//		var spyStringifyJSON = this.spy(listener.utils, 'stringifyJSON');
+	//		var spySendToQueue = this.spy(transport.channel, 'sendToQueue');
+	//		var spyAck = this.spy(transport.channel, 'ack');
+
+	//		// handle the message
+	//		listener.handleMessage(message, data);
+
+	//		/*
+	//		 * assertions
+	//		 */
+	//		stubHandleRequest.should.have.been.calledOnce();
+	//		spyStringifyJSON.should.have.been.calledOnce();
+	//		spyStringifyJSON.should.have.been.calledWithExactly(seneca, 'listen-amqp', data);
+	//		spySendToQueue.should.have.been.calledOnce();
+	//		spySendToQueue.should.have.been.calledWithExactly(message.properties.replyTo, new Buffer(JSON.stringify(data)), {
+	//			correlationId: message.properties.correlationId
+	//		});
+	//		spyAck.should.have.been.calledOnce();
+	//		spyAck.should.have.been.calledWithExactly(message);
+	//	}));
+	//});
+
+	//describe('listen()', function () {
+	//	it('should listen and consume messages from the channel', Sinon.test(function () {
+	//		// stubs
+	//		var spyConsume = this.stub(postmaster.listenerConn.channel, 'consume', function (queue, cb) {
+	//			// return the message
+	//			return cb(message);
+	//		});
+
+	//		listener.listenForMessages(postmaster.listenerConn, message);
+
+	//		spyConsume.should.have.been.calledOnce();
+	//	}));
+	//});
+
+	//describe('consume()', function () {
+	//	it('should not acknowledge messages without content', Sinon.test(function () {
+	//		var msg = {
+	//			properties: {
+	//				replyTo: 'seneca.res.r1FYNSEN'
+	//			}
+	//		};
+
+	//		var spyNack = this.spy(transport.channel, 'nack');
+	//		var spyHandleMessage = this.spy(listener, 'handleMessage');
+
+	//		// consume the message
+	//		listener.consume()(msg);
+
+	//		spyNack.should.have.been.calledOnce();
+	//		spyHandleMessage.should.not.have.been.called();
+	//	}));
+
+	//	it('should not acknowledge messages without a replyTo property', Sinon.test(function () {
+	//		var msg = {
+	//			properties: {}
+	//		};
+
+	//		var spyNack = this.spy(transport.channel, 'nack');
+	//		var spyHandleMessage = this.spy(listener, 'handleMessage');
+
+	//		// consume the message
+	//		listener.consume()(msg);
+
+	//		spyNack.should.have.been.calledOnce();
+	//		spyHandleMessage.should.not.have.been.called();
+	//	}));
+
+	//	it('should handle a valid message', Sinon.test(function () {
+	//		var spyNack = this.spy(transport.channel, 'nack');
+	//		var spyHandleMessage = this.spy(listener, 'handleMessage');
+	//		var spyParseJSON = this.spy(listener.utils, 'parseJSON');
+
+	//		// consume the message
+	//		listener.consume()(message);
+
+	//		spyNack.should.not.have.been.called();
+	//		spyParseJSON.should.have.been.calledOnce();
+	//		spyHandleMessage.should.have.been.calledOnce();
+	//	}));
+	//});
+});
+
+//describe('Unit tests for dead-letter module', function () {
+//	let channel = {
+//		assertQueue: (queue) => Promise.resolve({
+//			queue
+//		}),
+//		assertExchange: (exchange) => Promise.resolve({
+//			exchange
+//		}),
+//		bindQueue: () => Promise.resolve()
+//	};
+
+//	before(function () {
+//		// Create spies for channel methods
+//		Sinon.stub(channel, 'assertQueue', channel.assertQueue);
+//		Sinon.stub(channel, 'assertExchange', channel.assertExchange);
+//		Sinon.stub(channel, 'bindQueue', channel.bindQueue);
+//	});
+
+//	afterEach(function () {
+//		// Reset the state of the stub functions
+//		channel.assertQueue.reset();
+//		channel.assertExchange.reset();
+//		channel.bindQueue.reset();
+//	});
+
+//	describe('declareDeadLetter()', function () {
+//		it('should return a Promise', function () {
+//			DeadLetter.declareDeadLetter().should.be.instanceof(Promise);
+//		});
+
+//		it('should avoid any declaration if `options.queue` is not present', function (done) {
+//			var options = {
+//				exchange: {}
+//			};
+//			DeadLetter.declareDeadLetter(options, channel)
+//				.then(() => {
+//					Sinon.assert.notCalled(channel.assertQueue);
+//					Sinon.assert.notCalled(channel.assertExchange);
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should avoid any declaration if `options.exchange` is not present', function (done) {
+//			var options = {
+//				queue: {}
+//			};
+//			DeadLetter.declareDeadLetter(options, channel)
+//				.then(() => {
+//					Sinon.assert.notCalled(channel.assertQueue);
+//					Sinon.assert.notCalled(channel.assertExchange);
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should avoid any declaration if `channel` is null', function (done) {
+//			DeadLetter.declareDeadLetter(DEFAULT_OPTIONS, null)
+//				.then(() => {
+//					Sinon.assert.notCalled(channel.assertQueue);
+//					Sinon.assert.notCalled(channel.assertExchange);
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should declare a dead letter exchange', function (done) {
+//			DeadLetter.declareDeadLetter(DEFAULT_OPTIONS, channel)
+//				.then(() => {
+//					var opt = DEFAULT_OPTIONS.exchange;
+//					Sinon.assert.calledOnce(channel.assertExchange);
+//					Sinon.assert.calledWithExactly(channel.assertExchange, opt.name, opt.type, opt.options);
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should declare a dead letter queue', function (done) {
+//			DeadLetter.declareDeadLetter(DEFAULT_OPTIONS, channel)
+//				.then(() => {
+//					var opt = DEFAULT_OPTIONS.queue;
+//					Sinon.assert.calledOnce(channel.assertQueue);
+//					Sinon.assert.calledWithExactly(channel.assertQueue, opt.name, opt.options);
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should bind dead letter queue and exchange with "#" as routing key', function (done) {
+//			DeadLetter.declareDeadLetter(DEFAULT_OPTIONS, channel)
+//				.then(() => {
+//					Sinon.assert.calledOnce(channel.bindQueue);
+//					Sinon.assert.calledWithExactly(channel.bindQueue, DEFAULT_OPTIONS.queue.name, DEFAULT_OPTIONS.exchange.name, '#');
+//				})
+//				.asCallback(done);
+//		});
+
+//		it('should resolve to an object containing `dlq`, `dlx` and `rk` props', function (done) {
+//			DeadLetter.declareDeadLetter(DEFAULT_OPTIONS, channel)
+//				.then((dl) => {
+//					// Match `dlq` property to `options.queue.name`
+//					dl.should.have.property('dlq')
+//						.and.equal(DEFAULT_OPTIONS.queue.name);
+//					// Match `dlx` property to `options.exchange.name`
+//					dl.should.have.property('dlx')
+//						.and.equal(DEFAULT_OPTIONS.exchange.name);
+//					// Match 'rk' property to '#'
+//					dl.should.have.property('rk').and.equal('#');
+//				})
+//				.asCallback(done);
+//		});
+//	});
+//});
